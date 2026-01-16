5e5472e1f9d11d77d7956ba92e541a5a95fef8fc
LaCalmaInc <alvaro.guzman@baiko.cl>
2026-01-16 01:20:21 -0300

    styles/app-styles
    commit-id:896ab09d

--- React/udemy/my-code-react/pizza-project/src/App.tsx
+++ React/udemy/my-code-react/pizza-project/src/App.tsx
@@ -1,12 +1,14 @@
-import "./App.css";
+import  s from "./App.module.css";
 import { pizzaData } from "./data";
 import { Pizza } from "./components";
 function App() {
   const pizza1 = pizzaData[0];
   return (
     <>
-      <h1>Fast React Pizza Co.</h1>
-      <h2>Our Menu</h2>
+      <div className={s.header}>
+        <h1 >Fast React Pizza Co.</h1>
+        <h2>Our Menu</h2>
+      </div>
       <Pizza {...pizza1} />
     </>
   );
